f90d862f9ba47b4fc71412fecbb1da78760e519c
ADARSH TOMY <90443531+RamAgE700@users.noreply.github.com>
2024-10-01 06:05:31 +0530

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9n(rzvb1slxzt(&je0_4uagp-w_jffr3nd=nf)pi8ls=6@@8rg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['minisem3-1.onrender.com', 'localhost', '127.0.0.1']
 
 # settings.py
 
